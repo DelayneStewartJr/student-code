acdac910195d522700710943cde40852c0dbcf4a
Delayne Stewart Jr <dstewartjr1@gmail.com>
2023-05-25 13:34:21 -0400

    message

---
+++ module-1/09_Classes_and_Encapsulation/lecture/src/main/java/com/techelevator/Elevator.java
@@ -0,0 +1,16 @@
+package com.techelevator;
+
+public class Elevator {
+
+
+    private String elevatorTag;
+    private  int currentFloor;
+    private  int numberOfFloors;
+    private boolean doorOpen;
+
+
+
+
+
+
+
