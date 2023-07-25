a146770063e4a2e892bb476e98b3caf9462c2955
Delayne Stewart Jr <dstewartjr1@gmail.com>
2023-07-25 16:51:10 -0400

    intro to HTML resubmit

--- module-3/11_Intro_to_Vue_JS/lecture/src/App.vue
+++ module-3/11_Intro_to_Vue_JS/lecture/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <product-review/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import ProductReview from "./components/ProductReview.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    ProductReview
   }
 }
 </script>
---
+++ module-3/11_Intro_to_Vue_JS/tutorial/src/components/StudentInfo.vue
@@ -0,0 +1,30 @@
+<template>
+<div class= "student- display">
+    <h2>John Mueller<h2>
+<div>    
+</template>
+
+<script>
+export default {
+    name: "student-info"
+}
+</script>
+
+
+<style scoped>
+.student-display {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 10px 30px;
+    background-color: #220e9;
+    width: 40%;
+    border: 4px gold solid;
+    margin-bottom: 5px;
+}
+
+h2 {
+    color: gold;
+    font-size; 2em;
+}
+</style>
