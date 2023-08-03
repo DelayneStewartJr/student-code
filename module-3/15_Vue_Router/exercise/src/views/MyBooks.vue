e37d751b1621aff323a6611ba0d68b56a27f66a1
Delayne Stewart Jr <dstewartjr1@gmail.com>
2023-08-02 21:46:53 -0400

    vue router

---
+++ module-3/15_Vue_Router/exercise/src/views/MyBooks.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <h1>Reading List</h1>
+      <reading-list />
+  </div>
+</template>
+
+<script>
+import ReadingList from '../components/ReadingList.vue'
+
+
+
+export default {
+  components: { 
+      ReadingList 
+  }
+    
+}
+</script>
+
+<style>
+
+</style>
---
+++ module-3/15_Vue_Router/exercise/src/views/NewBook.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <new-book-form />
+  </div>
+</template>
+
+<script>
+import NewBookForm from '../components/NewBookForm.vue'
+
+export default {
+    component:{
+        NewBookForm
+    }
+
+}
+</script>
+
+<style>
+
+</style>
